e904b3e721a40ea4379765907cf86d51f336f023
xMagiJinx <115854187+xMagiJinx@users.noreply.github.com>
2022-10-18 09:31:06 -0400

    adding bullets and updating settings

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,8 @@
 import sys
 import pygame
 from setting import Settings
+from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -10,23 +12,72 @@ class AlienInvasion:
         pygame.init()
         self.setting = Settings()
 
+        # In the walkthrough he changes the screen to be fullscreen, I don't like fullscreen
         self.screen = pygame.display.set_mode(
             (self.setting.screen_width, self.setting.screen_height)
         )
+        self.setting.screen_width = self.screen.get_rect().width
+        self.setting.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
+        self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Start main loop for the game"""
         while True:
-            for event in pygame.event.get():
-                if event.type ==pygame.QUIT:
-                    sys.exit()
-            #watches for events from keyboard and mouse
+            self._check_events()
+            self.ship.update()
+            self._update_bullets()
+            self._update_screen()
 
-            self.screen.fill(self.setting.bg_color)
+    def _check_events(self):
+        """Respond to keypresses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT():
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
 
-            pygame.display.flip()
+    def _check_keydown_events(self):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = rue
+        elif event.key == pygame.k_q:
+            sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+
+    def _check_keyup_events(self, event):
+        """Respond to key releases"""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
+
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group"""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+    def _update_bullets(self):
+        self.bullets.update()
+
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <= 0:
+                self.bullets.remove(bullet)
+
+    def _update_screen(self):
+        """Update images on the screen and flip to new screen"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+        pygame.display.flip()
 
 if __name__ == '__main__':
     ai = AlienInvasion()
--- setting.py
+++ setting.py
@@ -2,6 +2,16 @@ class Settings:
     """Store all settings for Alien Invasion."""
     def __init__(self):
         """Initialize game settings"""
-        self.screen_width = 1200
-        self.screen_height = 800
-        self.bg_color = (230, 255, 255)
+        self.screen_width = 1000
+        self.screen_height = 600
+        self.bg_color = (230, 230, 230)
+
+        #ship settings
+        self.ship_speed = 1.5
+
+        #bullet settings
+        self.bullet_speed = 1.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+        self.bullet_allowed = 3
