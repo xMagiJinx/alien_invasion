9b6c470c9332e0cf2563e022041733cdd7154f7c
xMagiJinx <m255448@usna.edu>
2022-11-11 15:00:08 -0500

    Homeowkr 3_5

---
+++ homework2/13_5.py
@@ -0,0 +1,100 @@
+import sys
+from random import random
+
+import pygame
+
+from settings import Settings
+from ship import Ship
+from bullet import Bullet
+from alien import Alien
+
+
+class Shooter:
+    def __init__(self):
+        pygame.init()
+        self.settings = Settings()
+
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height)
+        )
+        pygame.display.set_caption("Shooter")
+
+        self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
+        self.aliens = pygame.sprite.Group()
+
+    def run_game(self):
+        while True:
+            self._check_events()
+            self._create_alien()
+            self.ship.update()
+            self._update_bullets()
+            self.aliens.update()
+            self._update_screen()
+
+    def _check_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        if event.key == pygame.K_UP:
+            self.ship.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        if event.key == pygame.K_UP:
+            self.ship.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.ship.moving_down = False
+
+    def _fire_bullet(self):
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        self.bullets.update()
+
+        for bullet in self.bullets.copy():
+            if bullet.rect.left >= self.screen.get_rect().right:
+                self.bullets.remove(bullet)
+
+        self._check_bullet_alien_collisions()
+
+    def _check_bullet_alien_collisions(self):
+        """Check if hit an alien"""
+        collisions = pygame.sprite.groupcollide(
+            self.bullets, self.aliens, True, True
+        )
+
+    def _create_alien(self):
+        """Make some aliens"""
+        if random() < self.settings.alien_appear:
+            alien = Alien(self)
+            self.aliens.add(alien)
+            print(len(self.aliens))
+
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
+
+        self.aliens.draw(self.screen)
+
+        pygame.display.flip()
+
+
+if __name__ == '__main__':
+    ss_game = Shooter()
+    ss_game.run_game()
--- homework2/settings.py
+++ homework2/settings.py
@@ -5,6 +5,18 @@ class Settings:
         """Init settings"""
         self.screen_width = 1000
         self.screen_height = 800
-        self.bg_color = (0,0,0)
+        self.bg_color = (255,255,255)
 
-        self.raindrop_speed = .5
+        self.ship_speed = 3
+
+        self.bullet_speed = 6.0
+        #going sideways so now the width is longer than height
+        self.bullet_width = 15
+        self.bullet_height = 3
+        self.bullet_color = (60, 60, 60)
+        self.bullets_allowed = 3
+
+        # alien settings
+        # make aliens appear at random
+        self.alien_appear = .008
+        self.alien_speed = .5
